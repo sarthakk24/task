08c4e12791b8fabd88385042ffda1aaccbf8dd86
Sarthak Sachdeva <sarthak.sachdeva.73@gmail.com>
2023-06-23 09:16:42 +0530

    initial routes

---
+++ components/shared/navbar.tsx
@@ -0,0 +1,6 @@
+const Navbar = () => {
+    return <nav className='bg-black text-white w-[20%] rounded-[30px] m-10'>nav</nav>
+}
+
+export default Navbar
+     
---
+++ pages/signin.tsx
@@ -0,0 +1,7 @@
+import Navbar from 'dashboard/components/shared/navbar'
+
+const SignIn = () => {
+    return <div className='h-screen flex'><Navbar/></div>
+}
+
+export default SignIn
