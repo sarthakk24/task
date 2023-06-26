ce7eaf4217de8d2fa34199be6d1848f8d907f323
Sarthak Sachdeva <sarthak.sachdeva.73@gmail.com>
2023-06-26 20:47:55 +0530

    add aws serverless compatible

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -4,66 +4,11 @@ import Products from "dashboard/components/home/products";
 import Schedule from "dashboard/components/home/schedule";
 import HorizontalBar from "dashboard/components/shared/horizontalBar";
 import VerticalBar from "dashboard/components/shared/verticalBar";
+import axios from "axios";
 
-const data = {
-  revenue: "2,129,430",
-  transaction: "1,520",
-  likes: "9,721",
-  users: "892",
-  tenure: "May - June 2021",
-  products: [
-    { name: "Basic Tees", value: 55 },
-    { name: "Custom Short pants", value: 31 },
-    { name: "Super Hoodies", value: 14 },
-  ],
-  schedule: [
-    {
-      task: "Meeting with suppliers from Kuta Bali",
-      time: "14.00-15.00",
-      location: "Sunset Road, Kuta, Bali ",
-    },
-    {
-      task: "Check operation at Giga Factory 1",
-      time: "18.00-20.00",
-      location: "Central Jakarta ",
-    },
-  ],
-  customers: ["Guests", "Users"],
-  metrics: [
-    {
-      name: "",
-      uv: 200,
-      pv: 100,
-    },
-    {
-      name: "Week 1",
-      uv: 390,
-      pv: 520,
-    },
-    {
-      name: "Week 2",
-      uv: 220,
-      pv: 150,
-    },
-    {
-      name: "Week 3",
-      uv: 300,
-      pv: 450,
-    },
-    {
-      name: "Week E",
-      uv: 210,
-      pv: 190,
-    },
-    {
-      name: "",
-      uv: 450,
-      pv: 300,
-    },
-  ],
-};
+const Dashboard = ({ resData }: any) => {
+  const { data } = resData;
 
-const Dashboard = () => {
   return (
     <div className=" lg:h-screen flex lg:p-10 flex-col lg:flex-row justify-between">
       <VerticalBar />
@@ -107,5 +52,18 @@ const Dashboard = () => {
     </div>
   );
 };
+
+export async function getServerSideProps() {
+  try {
+    const data = await axios.get(
+      `https://n5d80tw7u4.execute-api.ap-south-1.amazonaws.com/dev/data`
+    );
+    const resData = data.data.data;
+    return { props: { resData } };
+  } catch (error) {
+    console.log(error);
+    return { notFound: true };
+  }
+}
 
 export default Dashboard;
